61f31a5a4c8411251a8c77eb12cca9e5b5cf2df3
paoAracena <Paolaaracena1233@gmail.com>
2023-03-12 14:43:53 -0400

    Team Work

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-// window.onload = () => {
-//     let rightAnswer = document.getElementsByClassName("right-answer")[0]
-//     let wrongAnswer = document.getElementsByClassName("wrong-answer")[0]
-//     rightAnswer.onclick = rightAnswerClick
-//     wrongAnswer.onclick = wrongAnswerClick
-// }
+window.onload = () => {
+    let rightAnswers = document.getElementsByClassName("right-answer")
+    let wrongAnswers = document.getElementsByClassName("wrong-answer")
+    // rightAnswers.onclick = rightAnswerClick
+    // wrongAnswers.onclick = wrongAnswerClick
+    for (const rightAnswer of rightAnswers ) {
+        rightAnswer.onclick = rightAnswerClick
+    }
+    for (const wrongAnswer of wrongAnswers) {
+        wrongAnswer.onclick = wrongAnswerClick
+    }
+    console.log("right answer")
+}
+let rightAnswerClick = (event) => {
+    event.target.classList.add("green")
+}
 
-// let rightAnswerClick = (event) => {
-//     event.target.classList.add("green")
-// }
+let wrongAnswerClick = (event) => {
+    event.target.classList.add("red")
+}
 
-// let wrongAnswerClick = (event) => {
-//     event.target.classList.add("red")
-// }
+const element = document.querySelector(".answer-Box");
+
+element.addEventListener("mouseover", event => {
+  console.log("Mouse in");
+});
+element.addEventListener("mouseout", event => {
+    console.log("Mouse out");
+  });
